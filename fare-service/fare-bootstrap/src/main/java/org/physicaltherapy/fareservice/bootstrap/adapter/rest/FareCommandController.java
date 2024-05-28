63de5d59b1cbdbb41d4e73aa0f84c724bf5e83e9
habin <danny9643@naver.com>
2024-05-28 22:41:34 +0900

    configuration(package) fare
    Fare Service 삭제

--- fare-service/fare-bootstrap/src/main/java/org/physicaltherapy/fareservice/bootstrap/adapter/rest/FareCommandController.java
+++
@@ -1,7 +0,0 @@
-package org.physicaltherapy.fareservice.bootstrap.adapter.rest;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class FareCommandController {
-}
--- fare-service/fare-usecase/src/main/java/org/physicaltherapy/fareservice/usecase/service/FindFareService.java
+++
@@ -1,8 +0,0 @@
-package org.physicaltherapy.fareservice.usecase.service;
-
-import org.physicaltherapy.fareservice.usecase.port.input.FindFareUseCase;
-import org.springframework.stereotype.Service;
-
-@Service
-public class FindFareService implements FindFareUseCase {
-}
